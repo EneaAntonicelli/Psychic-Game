c4bdd33fb8f3923f8e879a26d13289d443b6e762
Enea <eneasecondary@gmail.com>
2018-06-17 00:57:41 -0700

    Attempting to make a hangman game. Direction currently unclear.

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+// Declare the wins variable
+var wins = 0;
+var madonna;
+var blankWord = "_ _ _ _ _ _ _"
+var btn = document.getElementsByClassName("btn");
+
+function  myFunction(){
+    
+    var name = prompt("What is your dumb name?");
+    var confirm = confirm("Hello" + name +". You ready for this shit?");
+};
+
+window.onload = myFunction();
+    document.btn.onclick = function myFunction();
+    
+    //validation code to see State field is mandatory.  
+}
+
+if (confirm) {
+    document.appendChild("h1", blankWord);
+}
+else {
+    document.write("Ok go fuck yourself then...")
+}
+
+// Now we want the computer to select a random word.
+
+// Now we want the computer to take the players guess.
+
+// Ensure that the player guess is an alphabetical letter
+
+// Make sure guesses are not repeatable
+
+// If the guessed letter is part of the word, add to 
+
+// document.getElementsByClassName("btn");
+// var start.onClick()
